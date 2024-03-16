05b05d9522af8970348631f3297bd255db68adb7
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 13:04:17 +0100

    chore: add mikro-orm module for database configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'my-db-name.sqlite3',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
