b3dffb8c43cba2be29cd4d89fa687ef295a1b3be
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 14:56:56 +0100

    chore: add mikro-orm middleware and user module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  OnModuleInit,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot()],
+  imports: [MikroOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule, OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MikroOrmMiddleware).forRoutes('*');
+  }
+
+  async onModuleInit() {
+    await this.orm.getMigrator().up();
+  }
+}
