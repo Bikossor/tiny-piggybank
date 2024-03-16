df2fcbed2f20aa43667ecf3f5a80df53c3a10da6
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 15:29:06 +0100

    chore: speciry database driver

--- api/mikro-orm.config.ts
+++ api/mikro-orm.config.ts
@@ -1,7 +1,8 @@
 import { Migrator } from '@mikro-orm/migrations';
-import { defineConfig } from '@mikro-orm/sqlite';
+import { SqliteDriver, defineConfig } from '@mikro-orm/sqlite';
 
 export default defineConfig({
+  driver: SqliteDriver,
   dbName: 'tiny-piggybank.sqlite3',
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
