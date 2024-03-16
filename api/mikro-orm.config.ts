25f6f2713a2f9bfa57de595b887ea1faa7064d65
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 13:32:31 +0100

    chore: add migrator extension to config

--- api/mikro-orm.config.ts
+++ api/mikro-orm.config.ts
@@ -1,7 +1,9 @@
+import { Migrator } from '@mikro-orm/migrations';
 import { defineConfig } from '@mikro-orm/sqlite';
 
 export default defineConfig({
   dbName: 'tiny-piggybank.sqlite3',
   entities: ['./dist/entities'],
   entitiesTs: ['./src/entities'],
+  extensions: [Migrator],
 });
