ee984ba893ee86b572536b288089842347490022
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 19:23:55 +0100

    chore: add OneToMany relationship for Piggybank in User entity

--- api/src/user/user.entity.ts
+++ api/src/user/user.entity.ts
@@ -1,9 +1,13 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, OneToMany, Property } from '@mikro-orm/core';
 import { BaseEntity } from 'src/base/base.entity';
 import { UserRepository } from './user.repository';
+import { Piggybank } from 'src/piggybank/piggybank.entity';
 
 @Entity({ repository: () => UserRepository })
 export class User extends BaseEntity {
   @Property()
   name: string;
+
+  @OneToMany('Piggybank', 'owner')
+  piggybanks = new Set<Piggybank>();
 }
