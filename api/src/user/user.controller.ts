8ba047c7d58618a36a13bf52acccbf8d0d87f329
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 21:03:57 +0100

    chore: add POST endpoint for creating a user

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/CreateUserDto';
 
 @Controller()
 export class UserController {
@@ -9,5 +10,10 @@ export class UserController {
   @Get('user')
   async getUser(): Promise<User[]> {
     return this.userService.getUser();
+  }
+
+  @Post('user')
+  async create(@Body('user') userData: CreateUserDto) {
+    return this.userService.create(userData);
   }
 }
