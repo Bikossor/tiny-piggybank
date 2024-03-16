408244a1a7d0170b025ab6c6b2c2175849cf8d5b
Bikossor <andre.lichtenthaeler@gmail.com>
2024-03-16 14:40:20 +0100

    chore: update entity paths

--- api/mikro-orm.config.ts
+++ api/mikro-orm.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@mikro-orm/sqlite';
 
 export default defineConfig({
   dbName: 'tiny-piggybank.sqlite3',
-  entities: ['./dist/entities'],
-  entitiesTs: ['./src/entities'],
+  entities: ['./dist/**/*.entity.js'],
+  entitiesTs: ['./src/**/*.entity.ts'],
   extensions: [Migrator],
 });
